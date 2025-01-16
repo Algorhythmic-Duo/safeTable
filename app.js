f32a5b65ae1b773a42b66f0c1d1172133c88d169
MathewsVinoy <mathewsvinoy@gmail.com>
2025-01-16 10:33:16 +0530

    getting started

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const PORT = 1315;
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.listen(PORT, (error) => {
+  if (!error) {
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  } else {
+    console.log("Error occurred, server can't start", error);
+  }
+});
